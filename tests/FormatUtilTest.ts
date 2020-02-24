1513e873eef3f9525f36550cfa75ea4f1c80b847
Half-Shot <will@half-shot.uk>
2020-02-24 13:08:08 +0000

    Remove notif test

--- tests/FormatUtilTest.ts
+++ tests/FormatUtilTest.ts
@@ -1,6 +1,5 @@
 import { FormatUtil } from "../src/FormatUtil";
 import { expect } from "chai";
-import { UserNotification } from "../src/UserNotificationWatcher";
 
 const SIMPLE_ISSUE = {
     number: 123,
@@ -8,26 +7,6 @@ const SIMPLE_ISSUE = {
     title: "A simple title",
     html_url: "https://github.com/evilcorp/lab/issues/123",
     repository_url: "https://api.github.com/repos/evilcorp/lab",
-};
-
-const SIMPLE_NOTIFICATION: UserNotification = {
-    id: "",
-    reason: "assign",
-    unread: true,
-    updated_at: 0,
-    last_read_at: 0,
-    url: "",
-    subject: {
-        title: "",
-        url: "",
-        latest_comment_url: "",
-        type: "Issue",
-        url_data: null,
-        latest_comment_url_data: null,
-    },
-    repository: {
-
-    } as any
 };
 
 describe("FormatUtilTest", () => {
@@ -38,11 +17,6 @@ describe("FormatUtilTest", () => {
     });
     it("correctly formats a room topic", () => {
         expect(FormatUtil.formatRoomTopic(SIMPLE_ISSUE)).to.equal(
-            "A simple title | Status: open | https://github.com/evilcorp/lab/issues/123",
-        );
-    });
-    it("correctly formats a notification", () => {
-        expect(FormatUtil.formatNotification(SIMPLE_NOTIFICATION)).to.equal(
             "A simple title | Status: open | https://github.com/evilcorp/lab/issues/123",
         );
     });
