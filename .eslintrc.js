bd6838aff5466e4eab2991b04b3c3b04233ff7af
Will Hunt <willh@matrix.org>
2020-11-22 21:10:10 +0000

    Lint

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: [
+        '@typescript-eslint',
+        'mocha'
+    ],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    rules: {
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+        "camelcase": ["error", { "properties": "never", "ignoreDestructuring": true }],
+    },
+    "env": {
+        "node": true,
+        "es6": true,
+    },
+};
