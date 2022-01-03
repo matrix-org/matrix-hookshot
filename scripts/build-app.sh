eddb599d302a850437a32e8872cb9cf0b328ab72
Half-Shot <will@half-shot.uk>
2022-01-03 23:48:47 +0000

    Add debugging again

--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -3,11 +3,14 @@
 # exit when any command fails
 set -e
 
-echo "Building Rust layer"
-yarn run build:app:rs
 echo "Building web"
 yarn run build:web
+echo "Building Rust layer"
+yarn run build:app:rs
 echo "Running rust-typescript definitions fix"
 yarn run build:app:fix-defs
+echo "#### NEW TYPES"
+cat src/libRs.d.ts
+echo "#### END TYPES"
 echo "Building Typescript layer"
 yarn run build:app
