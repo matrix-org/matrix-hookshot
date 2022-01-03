ebbd1c25593f290931497c11360681e7ed65a77a
Half-Shot <will@half-shot.uk>
2022-01-03 23:38:35 +0000

    Move the web build between the rust steps.
    I'm committing a sin here in that we want to avoid a race between these
    two steps, so sticking a unrelated build step in the middle. It's an
    annoying artefact of the napi build step where the command exits before
    the file is written, causing ts-node to error later on.

--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -3,10 +3,10 @@
 # exit when any command fails
 set -e
 
-echo "Building web"
-yarn run build:web
 echo "Building Rust layer"
 yarn run build:app:rs
+echo "Building web"
+yarn run build:web
 echo "Running rust-typescript definitions fix"
 yarn run build:app:fix-defs
 echo "Building Typescript layer"
