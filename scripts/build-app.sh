8d8fc2297171233e92f4f04034771bfb3bf0d8dd
Half-Shot <will@half-shot.uk>
2022-01-03 23:17:44 +0000

    temp: export the result of the types build

--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -7,6 +7,7 @@ echo "Building web"
 yarn run build:web
 echo "Building Rust layer"
 yarn run build:app:rs
+cat src/libRs.d.ts
 echo "Running rust-typescript definitions fix"
 yarn run build:app:fix-defs
 echo "Building Typescript layer"
