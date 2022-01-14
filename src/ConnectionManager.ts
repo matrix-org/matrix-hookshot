178b0846c70bf32cac4e4d34b5fbd503216e7010
Half-Shot <will@half-shot.uk>
2022-01-14 17:25:15 +0000

    Fixup error

--- src/ConnectionManager.ts
+++ src/ConnectionManager.ts
@@ -89,7 +89,7 @@ export class ConnectionManager {
                 throw Error('GitHub is not configured');
             }
             if (!this.config.checkPermission(userId, "github", BridgePermissionLevel.manageConnections)) {
-                throw new ApiError('User is not permitted to provision connections for Jira', ErrCode.ForbiddenUser);
+                throw new ApiError('User is not permitted to provision connections for GitHub', ErrCode.ForbiddenUser);
             }
             const res = await GitHubRepoConnection.provisionConnection(roomId, userId, data, this.as, this.tokenStore, this.github, this.config.github);
             await this.as.botIntent.underlyingClient.sendStateEvent(roomId, GitHubRepoConnection.CanonicalEventType, res.connection.stateKey, res.stateEventContent);
