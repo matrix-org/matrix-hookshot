83cef1d75802a91da6f093ed37b35ffbe2bc1b96
Half-Shot <Half-Shot@users.noreply.github.com>
2022-03-30 10:36:54 +0000

    deploy: 3cf1552ea7d8f5a38d3b595d9654240ed8401909

---
+++ latest/_site/main.js
@@ -0,0 +1,36 @@
+window.addEventListener("load", () => {
+    const scrollbox = document.querySelector(".sidebar-scrollbox");
+    scrollbox.innerHTML = `<div class="version-box"><span>Version: </span></div>${scrollbox.innerHTML}`;
+    const currentVersion = window.HOOKSHOT_VERSION || 'latest';
+
+    const selectElement = document.createElement("select");
+    
+    fetch("https://api.github.com/repos/matrix-org/matrix-hookshot/releases", {
+        cache: "force-cache",
+    }).then(res => 
+        res.json()
+    ).then(releases => {
+        selectElement.innerHTML = "";
+        for (const version of ['latest', ...releases.map(r => r.tag_name).filter(s => s !== "0.1.0")]) {
+            const option = document.createElement("option");
+            option.innerHTML = version;
+            selectElement.add(option);
+            if (currentVersion === version) {
+                option.setAttribute('selected', '');
+            }
+        }
+    }).catch(ex => {
+        console.error("Failed to fetch version data", ex);
+    })
+    
+    const option = document.createElement("option");
+    option.innerHTML = 'loading...';
+    selectElement.add(option);
+
+    selectElement.addEventListener('change', (event) => {
+        const versionlessPath = window.location.pathname.split('/').slice(2).join('/');
+        window.location = `${window.location.origin}/${event.target.value}/${versionlessPath}`;
+    });
+
+    document.querySelector(".version-box").appendChild(selectElement);
+});
