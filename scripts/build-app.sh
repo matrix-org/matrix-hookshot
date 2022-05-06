97107104ff4eca7b37be0eee1fe582040a1ed02f
Half-Shot <will@half-shot.uk>
2022-05-06 14:49:31 +0100

    Add workaround to build images foo crypto-nodejs

--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -3,6 +3,12 @@
 # exit when any command fails
 set -e
 
+# Workaround for non-x64 / ia32 targets
+pushd node_modules/@turt2live/matrix-sdk-crypto-nodejs
+echo "Checking matrix-sdk-crypto-nodejs bindings (and building if required)"
+node check-exists.js
+popd
+
 echo "Building Rust layer"
 yarn run build:app:rs
 echo "Running rust-typescript definitions fix"
