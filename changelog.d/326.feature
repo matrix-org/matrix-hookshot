bc29b784423ac20eadacea0d17818c84814c7d46
Will Hunt <will@half-shot.uk>
2022-05-09 12:16:27 +0100

    Allow overriding the results of an openID lookup for widgets for local development. (#326)
    * Revert "Withdraw openid overrides change for this PR"
    
    This reverts commit 561fb20471107827a5304eb3d4c4254bb215ae8d.
    
    * Drop setting from sample config
    
    * changelog
    
    * Clarify config

---
+++ changelog.d/326.feature
@@ -0,0 +1,1 @@
+Add `widgets.openIdOverrides` config option for developers to statically define server name <-> federation endpoints for openId lookups.
--- src/Config/Config.ts
+++ src/Config/Config.ts
@@ -271,6 +271,7 @@ interface BridgeWidgetConfigYAML {
     branding?: {
         widgetTitle: string,
     }
+    openIdOverrides?: Record<string, string>;
 }
 
 export class BridgeWidgetConfig {
@@ -283,6 +284,9 @@ export class BridgeWidgetConfig {
     public readonly branding: {
         widgetTitle: string,
     }
+
+    @hideKey()
+    public readonly openIdOverrides?: Record<string, URL>;
     constructor(yaml: BridgeWidgetConfigYAML) {
         this.addToAdminRooms = yaml.addToAdminRooms || false;
         this.disallowedIpRanges = yaml.disallowedIpRanges;
@@ -297,6 +301,12 @@ export class BridgeWidgetConfig {
         this.branding = yaml.branding || {
             widgetTitle: "Hookshot Configuration"
         };
+        if (yaml.openIdOverrides) {
+            this.openIdOverrides = {};
+            for (const [serverName, urlStr] of Object.entries(yaml.openIdOverrides)) {
+                this.openIdOverrides[serverName] = new URL(urlStr);
+            }
+        }
     }
 }
 
@@ -522,6 +532,10 @@ export class BridgeConfig {
         const hasWidgetListener = !!this.listeners.find(l => l.resources.includes('widgets'));
         if (this.widgets && !hasWidgetListener) {
             throw new ConfigError(`listeners`, "You have enabled the widgets feature, but not included a widgets listener.");
+        }
+
+        if (this.widgets && this.widgets.openIdOverrides) {
+            log.warn("The `widgets.openIdOverrides` config value SHOULD NOT be used in a production environment.")
         }
     }
 
