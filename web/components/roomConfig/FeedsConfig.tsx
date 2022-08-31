63ab457b453e28b71d63917db305a8865b40eb2c
Will Hunt <will@half-shot.uk>
2022-08-31 14:01:43 +0100

    Refactor feed errors and error presentation (#459)
    * Add support for setting a global feed timeout
    
    * Track failures, and don't show the first non-serious error
    
    * Tidy up error reporting
    
    * Make a prettier error pane
    
    * Use the prettier event pane
    
    * Show failed feed attempts in the widget
    
    * Ensure we catch connection resets
    
    * Add styling file
    
    * Ensure we only track one result per fetch
    
    * changelog
    
    * Refactor to use better status codes
    
    * Make feed results cheaper
    
    * splice

--- web/components/roomConfig/FeedsConfig.tsx
+++ web/components/roomConfig/FeedsConfig.tsx
@@ -4,8 +4,27 @@ import { BridgeConfig } from "../../BridgeAPI";
 import { FeedConnectionState, FeedResponseItem } from "../../../src/Connections/FeedConnection";
 import { ConnectionConfigurationProps, RoomConfig } from "./RoomConfig";
 import { Button, ButtonSet, InputField } from "../elements";
+import styles from "./FeedConnection.module.scss";
 
 import FeedsIcon from "../../icons/feeds.png";
+
+const FeedRecentResults: FunctionComponent<{item: FeedResponseItem}> = ({ item }) => {
+    if (!item.secrets) {
+        return null;
+    }
+    return <>
+        <h3>Recent feed results</h3>
+        {!item.secrets.lastResults.length && <span>There have been no recent updates for this feed.</span>}
+        <ul>
+            {item.secrets.lastResults.map(item => <li styles={styles.resultListItem} key={item.timestamp}>
+                {new Date(item.timestamp).toLocaleString()}: 
+                {item.ok && `✅ Successful fetch`}
+                {!item.ok && `⚠️ ${item.error}`}
+            </li>
+            )}
+        </ul>
+    </>;
+}
 
 const ConnectionConfiguration: FunctionComponent<ConnectionConfigurationProps<ServiceConfig, FeedResponseItem, FeedConnectionState>> = ({existingConnection, onSave, onRemove}) => {
     const urlRef = createRef<HTMLInputElement>();
@@ -27,15 +46,20 @@ const ConnectionConfiguration: FunctionComponent<ConnectionConfigurationProps<Se
     }, [canEdit, onSave, urlRef, labelRef, existingConnection]);
 
     return <form onSubmit={handleSave}>
-        <InputField visible={!existingConnection} label="URL" noPadding={true}>
-            <input ref={urlRef} disabled={!canEdit} placeholder="Feed URL" type="text" value={existingConnection?.config.url} />
-            <input ref={labelRef} disabled={!canEdit} placeholder="Label (optional)" type="text" value={existingConnection?.config.label} />
-        </InputField>
+        { existingConnection && <FeedRecentResults item={existingConnection} />}
 
+        <InputField visible={!existingConnection} label="URL" noPadding={true}>
+            <input ref={urlRef} disabled={!canEdit} type="text" value={existingConnection?.config.url} />
+        </InputField>
+        <InputField visible={!existingConnection} label="Label" noPadding={true}>
+            <input ref={labelRef} disabled={!canEdit} type="text" value={existingConnection?.config.label} />
+        </InputField>
+        
         <ButtonSet>
             { canEdit && <Button type="submit">{ existingConnection ? "Save" : "Subscribe" }</Button>}
             { canEdit && existingConnection && <Button intent="remove" onClick={onRemove}>Unsubscribe</Button>}
         </ButtonSet>
+
     </form>;
 };
 
