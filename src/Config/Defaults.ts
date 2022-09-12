db08bf83c7988e1a982c838966314b5d9fe539c8
Half-Shot <will@half-shot.uk>
2022-09-12 13:43:15 +0100

    Drop pointless default

--- src/Config/Defaults.ts
+++ src/Config/Defaults.ts
@@ -111,7 +111,6 @@ export const DefaultConfig = new BridgeConfig({
     feeds: {
         enabled: false,
         pollIntervalSeconds: 600,
-        pollTimeoutSeconds: 30,
     },
     provisioning: {
         secret: "!secretToken"
