f883d9ff912928b436994dbe41505d025e59a77c
Half-Shot <will@half-shot.uk>
2022-09-16 10:54:18 +0100

    Remove our one usage of sans

--- web/index.tsx
+++ web/index.tsx
@@ -1,7 +1,6 @@
 import { h, render } from 'preact';
 import 'preact/devtools';
 import App from './App';
-import "@fontsource/open-sans/400.css";
 import "./fonts/fonts.scss"
 import "./styling.scss";
 
