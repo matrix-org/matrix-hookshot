763c511fe5477ea77975397b05aad716adc70f75
Half-Shot <will@half-shot.uk>
2022-10-10 11:18:15 +0100

    Fix padding issue

--- web/components/elements/InputField.tsx
+++ web/components/elements/InputField.tsx
@@ -2,15 +2,21 @@ import { h, FunctionComponent } from "preact";
 import style from "./InputField.module.scss";
 
 interface Props {
+    className: string;
     visible?: boolean;
     label?: string;
     noPadding: boolean;
     innerChild?: boolean;
 }
 
-export const InputField: FunctionComponent<Props> = ({ children, visible = true, label, noPadding, innerChild = false }) => {
-    return visible ? <div className={style.inputField}>
-        {label && <label className={noPadding ? style.nopad : ""}>{innerChild && children}{label}</label>}
+export const InputField: FunctionComponent<Props> = ({ className, children, visible = true, label, noPadding, innerChild = false }) => {
+    const inputClassName = [
+        className,
+        style.inputField,
+        noPadding && style.nopad
+    ].filter(a => !!a).join(' ');
+    return visible ? <div className={inputClassName}>
+        {label && <label>{innerChild && children}{label}</label>}
         {(!label || !innerChild) && children}
     </div> : <></>
 };
