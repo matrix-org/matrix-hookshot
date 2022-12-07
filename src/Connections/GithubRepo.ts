32386dc154a805df691f540e7b3aa5f6265bb9f3
Half-Shot <will@half-shot.uk>
2022-12-07 11:25:32 +0000

    Apply changes to all connection types

--- src/Connections/GithubRepo.ts
+++ src/Connections/GithubRepo.ts
@@ -1247,7 +1247,7 @@ ${event.release.body}`;
 
     public async provisionerUpdateConfig(userId: string, config: Record<string, unknown>) {
         // Apply previous state to the current config, as provisioners might not return "unknown" keys.
-        const newState = { ...this.state, config };
+        const newState = { ...this.state, ...config };
         const validatedConfig = GitHubRepoConnection.validateState(newState);
         await this.as.botClient.sendStateEvent(this.roomId, GitHubRepoConnection.CanonicalEventType, this.stateKey, validatedConfig);
         this.state = validatedConfig;
