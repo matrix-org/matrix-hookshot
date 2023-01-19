03d6ed21eacf79895c73b1dd0d67cdcf5dd9bc0b
Will Hunt <will@half-shot.uk>
2023-01-19 10:33:52 +0000

    Update appservice.ts

--- src/appservice.ts
+++ src/appservice.ts
@@ -40,8 +40,8 @@ export function getAppservice(config: BridgeConfig, registration: IAppserviceReg
         intentOptions: {
             // If encryption support is enabled, we cannot expire Intent objects or we risk
             // a resource contention on the Sled DB.
-            maxCached: config.encryption && 0,
-            maxAgeMs: config.encryption && 0,
+            maxCached: config.encryption ? 0 : undefined,
+            maxAgeMs: config.encryption ? 0 : undefined,
             encryption: !!config.encryption,
         },
         cryptoStorage: cryptoStorage,
