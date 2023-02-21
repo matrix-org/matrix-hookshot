fdf34946cfe940a3dd2b34de882a7fffe4f82eed
Tadeusz Sośnierz <tadeusz@sosnierz.com>
2023-02-21 18:18:48 +0100

    Add the ability to migrate Github repo connections from go-neb

--- src/Widgets/GoNebMigrator.ts
+++ src/Widgets/GoNebMigrator.ts
@@ -1,18 +1,41 @@
 import { Logger } from "matrix-appservice-bridge";
 import axios from "axios";
 
-import { FeedConnection, FeedConnectionState } from "../Connections";
+import { FeedConnection, FeedConnectionState, GitHubRepoConnection, GitHubRepoConnectionState } from "../Connections";
+import { AllowedEvents as GitHubAllowedEvents, AllowedEventsNames as GitHubAllowedEventsNames } from "../Connections/GithubRepo";
 
 const log = new Logger("GoNebMigrator");
 
 interface MigratedConnections {
-    [FeedConnection.ServiceCategory]?: FeedConnectionState[]|undefined,
+    [FeedConnection.ServiceCategory]: FeedConnectionState[]|undefined,
+    [GitHubRepoConnection.ServiceCategory]: GitHubRepoConnectionState[]|undefined;
 }
 
 interface GoNebFeedsConfig {
     [url: string]: {
         rooms: string[],
     }
+}
+
+interface GoNebGithubRepos {
+    [githubPath: string]: {
+        Events: string[], // push, issues, pull_request, more?
+    }
+}
+
+interface GoNebService {
+    Type: string;
+    Config: any;
+}
+
+interface GoNebGithubWebhookService extends GoNebService {
+    Type: 'github-webhook';
+    Config: {
+        ClientUserID: string;
+        Rooms: {
+            [roomId: string]: { Repos: GoNebGithubRepos; }
+        }
+    };
 }
 
 export class GoNebMigrator {
@@ -35,17 +58,47 @@ export class GoNebMigrator {
         return feedsPerRoom;
     }
 
-    public async getConnectionsForRoom(roomId: string): Promise<MigratedConnections> {
+    static convertGithub(roomRepos: GoNebGithubRepos): GitHubRepoConnectionState[] {
+        const eventMapping: { [goNebEvent: string]: GitHubAllowedEventsNames } = {
+            'pull_request': 'pull_request',
+            'issues': 'issue',
+            // 'push': ???
+        };
+        return Object.entries(roomRepos).map(([githubPath, { Events }]) => {
+            const [org, repo] = githubPath.split('/');
+            const enableHooks = Events.map(goNebEvent => eventMapping[goNebEvent]).filter(e => !!e);
+
+            return {
+                org,
+                repo,
+                enableHooks,
+            };
+        });
+    }
+
+    public async getConnectionsForRoom(roomId: string, userId: string): Promise<MigratedConnections> {
         const feeds: FeedConnectionState[] = [];
+        const github: GitHubRepoConnectionState[] = [];
 
         for (const id of this.serviceIds) {
             const endpoint = this.apiUrl + (this.apiUrl.endsWith('/') ? '' : '/') + 'admin/getService';
             const res = await axios.post(endpoint, { 'Id': id });
-            const obj = res.data;
+            const obj = res.data as GoNebService;
             switch (obj.Type) {
                 case 'rssbot': {
                     const roomFeeds = GoNebMigrator.convertFeeds(obj.Config.feeds).get(roomId) ?? [];
                     feeds.push(...roomFeeds);
+                    break;
+                };
+                case 'github-webhook': {
+                    const service = obj as GoNebGithubWebhookService;
+                    if (service.Config.ClientUserID === userId) {
+                        const roomRepos = service.Config.Rooms[roomId]?.Repos;
+                        if (roomRepos) {
+                            const githubConnections = GoNebMigrator.convertGithub(roomRepos);
+                            github.push(...githubConnections);
+                        }
+                    }
                     break;
                 };
                 default: {
@@ -56,6 +109,7 @@ export class GoNebMigrator {
 
         return {
             feeds,
+            github,
         };
     }
 }
