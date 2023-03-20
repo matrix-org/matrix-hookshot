0a43da04a6ea1da527e4ee2026aeac7809c4f60b
Half-Shot <will@half-shot.uk>
2023-03-20 17:47:02 +0000

    Show a sensible message for pending GitHub installations

--- web/oauth.tsx
+++ web/oauth.tsx
@@ -3,6 +3,7 @@ import "./styling.scss";
 import "./oauth.scss";
 import { render } from 'preact';
 import 'preact/devtools';
+import type { OAuthPageParams } from '../src/Webhooks';
 
 const root = document.getElementsByTagName('main')[0];
 
@@ -15,24 +16,41 @@ const ServiceToName: Record<string,string> = {
 
 function RenderOAuth() {
     const params = new URLSearchParams(window.location.search);
-    const service = params.get('service') ?? 'default';
-    const error = params.get('error');
-    const errcode = params.get('errcode');
-    const oauthKind = params.get('oauth-kind') ?? 'account';
+    const service = (params.get('service') as OAuthPageParams['service']) ?? 'default';
+    const error = (params.get('error') as OAuthPageParams['error']);
+    const errcode = (params.get('errcode') as OAuthPageParams['errcode']);
+    const oauthKind = (params.get('oauth-kind') as OAuthPageParams['oauth-kind']) ?? 'account';
+    const result = (params.get('result') as OAuthPageParams['result']);
 
-    if (error) {
+    const serviceName = ServiceToName[service];
+
+    if (result === 'error') {
         return <>
-            <h1>Could not connect your { ServiceToName[service] } {oauthKind} to Hookshot.</h1>
+            <h1>Could not connect your { serviceName } {oauthKind} to Hookshot.</h1>
             <p>
                 <code>{errcode}</code> {error}
             </p>
         </>;
+    } else if (result === 'pending') {
+        return <>
+            <h1>Your connection to { serviceName } {oauthKind} is pending.</h1>
+            <p>
+                The owner may need to approve this.
+            </p>
+        </>;
+    } else if (result === 'success') {
+        return <>
+            <h1>Your { serviceName } {oauthKind} has been connected.</h1>
+            <p>You may close this window.</p>
+        </>;
     }
-
     return <>
-        <h1>Your { ServiceToName[service] } {oauthKind} has been connected.</h1>
-        <p>You may close this window.</p>
+        <h1>Your connection to { serviceName } {oauthKind} is {result}.</h1>
+        <p>
+            This is an unknown state, you may need to contact your systems administrator.
+        </p>
     </>;
+
 }
 
 if (root) {
