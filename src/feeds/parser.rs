6e9c54832b47339aab70b24a55cc03b935a408bc
Half-Shot <will@half-shot.uk>
2023-04-11 17:28:51 +0100

    Lint rust

--- src/feeds/parser.rs
+++ src/feeds/parser.rs
@@ -1,7 +1,7 @@
 use std::str::FromStr;
 
+use napi::bindgen_prelude::{Error as JsError, Status};
 use rss::{Channel, Error as RssError};
-use napi::{bindgen_prelude::{Error as JsError, Status}};
 
 #[derive(Serialize, Debug, Deserialize)]
 #[napi(object)]
@@ -19,48 +19,38 @@ pub struct JsRssChannel {
     pub items: Vec<FeedItem>,
 }
 
-
 #[napi(js_name = "parseRSSFeed")]
 pub fn js_parse_rss_feed(xml: String) -> Result<JsRssChannel, JsError> {
-    Channel::from_str(&xml).map(|channel| {
-        JsRssChannel {
+    Channel::from_str(&xml)
+        .map(|channel| JsRssChannel {
             title: channel.title().to_string(),
-            items: channel.items().iter().map(|item| {
-                FeedItem {
+            items: channel
+                .items()
+                .iter()
+                .map(|item| FeedItem {
                     title: item.title().map(|f| f.to_string()),
                     link: item.link().map(|f| f.to_string()),
                     id: item.guid().map(|f| f.value().to_string()),
                     id_is_permalink: item.guid().map_or(false, |f| f.is_permalink()),
-                }
-            }).collect()
-        }
-    }).map_err(|op| {
-        match op {
-            RssError::Utf8(err) => {
-                JsError::new(
-                    Status::Unknown,
-                    format!("An error while converting bytes to UTF8. {}'", err).to_string(),
-                )
-            }
-            RssError::Xml(err) => {
-                JsError::new(
-                    Status::Unknown,
-                    format!("XML parsing error. {}", err).to_string(),
-                )
-            }
-            RssError::InvalidStartTag => {
-                JsError::new(
-                    Status::Unknown,
-                    format!("The input didn't begin with an opening <rss> tag.").to_string(),
-                )
-            }
-            err => {
-                JsError::new(
-                    Status::Unknown,
-                    format!("Unknown error trying to parse feed parse feed '{}'", err).to_string(),
-                )
-            }
-        }
-        
-    })
+                })
+                .collect(),
+        })
+        .map_err(|op| match op {
+            RssError::Utf8(err) => JsError::new(
+                Status::Unknown,
+                format!("An error while converting bytes to UTF8. {}'", err).to_string(),
+            ),
+            RssError::Xml(err) => JsError::new(
+                Status::Unknown,
+                format!("XML parsing error. {}", err).to_string(),
+            ),
+            RssError::InvalidStartTag => JsError::new(
+                Status::Unknown,
+                format!("The input didn't begin with an opening <rss> tag.").to_string(),
+            ),
+            err => JsError::new(
+                Status::Unknown,
+                format!("Unknown error trying to parse feed parse feed '{}'", err).to_string(),
+            ),
+        })
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 pub mod Config;
 pub mod Github;
 pub mod Jira;
-pub mod format_util;
 pub mod feeds;
+pub mod format_util;
 
 #[macro_use]
 extern crate napi_derive;
