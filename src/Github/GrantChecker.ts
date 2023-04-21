c6a04c2ebddc9df27da4e01c9e8b5d84c135e712
Will Hunt <will@half-shot.uk>
2023-04-21 10:27:57 +0100

    Fix case where GitHub repos aren't connectable if the GitHub App was manually approved (#718)
    * Fix GitHub connections failing if the installation cache is stale
    
    * changelog

--- src/Github/GrantChecker.ts
+++ src/Github/GrantChecker.ts
@@ -2,7 +2,6 @@ import { Appservice } from "matrix-bot-sdk";
 import { GitHubRepoConnection } from "../Connections";
 import { GrantChecker } from "../grants/GrantCheck";
 import { UserTokenStore } from "../UserTokenStore";
-import { GithubInstance } from "./GithubInstance";
 import { Logger } from 'matrix-appservice-bridge';
 
 const log = new Logger('GitHubGrantChecker');
@@ -14,7 +13,7 @@ interface GitHubGrantConnectionId {
 
 
 export class GitHubGrantChecker extends GrantChecker<GitHubGrantConnectionId> {
-    constructor(private readonly as: Appservice, private readonly github: GithubInstance, private readonly tokenStore: UserTokenStore) {
+    constructor(private readonly as: Appservice, private readonly tokenStore: UserTokenStore) {
         super(as.botIntent, "github")
     }
 
@@ -29,7 +28,7 @@ export class GitHubGrantChecker extends GrantChecker<GitHubGrantConnectionId> {
             return true;
         }
         try {
-            await GitHubRepoConnection.assertUserHasAccessToRepo(sender, connectionId.org, connectionId.repo, this.github, this.tokenStore);
+            await GitHubRepoConnection.assertUserHasAccessToRepo(sender, connectionId.org, connectionId.repo, this.tokenStore);
             return true;
         } catch (ex) {
             log.info(`Tried to check fallback for ${roomId}: ${sender} does not have access to ${connectionId.org}/${connectionId.repo}`, ex);
