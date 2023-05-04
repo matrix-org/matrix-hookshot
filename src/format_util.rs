727d582e669350f669eaf68a76e3c12b75ee0408
Half-Shot <will@half-shot.uk>
2023-05-04 10:18:02 +0100

    Add a function to sanitize html output

--- src/format_util.rs
+++ src/format_util.rs
@@ -7,6 +7,7 @@ use napi::bindgen_prelude::*;
 use napi_derive::napi;
 use rgb::RGB;
 use std::fmt::Write;
+use ammonia::{Builder, UrlRelative};
 
 #[derive(Serialize, Debug, Deserialize)]
 #[napi(object)]
@@ -188,4 +189,66 @@ pub fn hash_id(id: String) -> Result<String> {
     let mut hasher = Md5::new();
     hasher.input(id);
     Ok(hex::encode(hasher.result()))
+}
+
+const ALLOWED_HTML_TAGS: &'static [&'static str] = &[
+    "font", "del", "h1", "h2", "h3", "h4", "h5", "h6", "blockquote",
+    "p", "a", "ul", "ol", "sup", "sub", "li", "b", "i", "u", "strong",
+    "em", "strike", "code", "hr", "br", "div", "table", "thead", "tbody",
+    "tr", "th", "td", "caption", "pre", "span", "img", "details", "summary"];
+
+const FONT_ALLOWED_ATTRIBUTES: &'static [&'static str] = &[
+    "data-mx-bg-color", "data-mx-color", "color"
+];
+
+const SPAN_ALLOWED_ATTRIBUTES: &'static [&'static str] = &[
+    "data-mx-bg-color", "data-mx-color", "data-mx-spoiler"
+];
+
+const ANCHOR_ALLOWED_ATTRIBUTES: &'static [&'static str] = &[
+    "name", "target", "href"
+];
+
+const IMG_ALLOWED_ATTRIBUTES: &'static [&'static str] = &[
+    "width", "height", "alt", "title", "src"
+];
+
+const OL_ALLOWED_ATTRIBUTES: &'static [&'static str] = &[
+    "start"
+];
+const CODE_ALLOWED_ATTRIBUTES: &'static [&'static str] = &[
+    "class"
+];
+
+const ALLOWED_URL_SCHEMES: &'static [&'static str] = &[
+    "https", "http", "ftp", "mailto", "magnet", "mxc"
+];
+
+#[napi]
+pub fn sanitize_html(html: String)-> String {
+    // See https://spec.matrix.org/v1.6/client-server-api/#mroommessage-msgtypes
+    Builder::default()
+    .link_rel(None)
+    .add_tags(ALLOWED_HTML_TAGS)
+    .add_tag_attributes("font", FONT_ALLOWED_ATTRIBUTES)
+    .add_tag_attributes("span", SPAN_ALLOWED_ATTRIBUTES)
+    .add_tag_attributes("a", ANCHOR_ALLOWED_ATTRIBUTES)
+    .add_tag_attributes("img", IMG_ALLOWED_ATTRIBUTES)
+    .add_tag_attributes("ol", OL_ALLOWED_ATTRIBUTES)
+    .add_tag_attributes("code", CODE_ALLOWED_ATTRIBUTES)
+    .add_url_schemes(ALLOWED_URL_SCHEMES)
+    .attribute_filter(|element, attribute, value| {
+        match (element, attribute) {
+            ("code", "class") => {
+                if value.starts_with("language-") {
+                    return Some(value.into());
+                }
+                None
+            },
+            _ => Some(value.into())
+        }
+    })
+    .url_relative(UrlRelative::PassThrough)
+    .clean(html.as_str())
+    .to_string()
 }
