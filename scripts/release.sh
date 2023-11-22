79e9214ae54e6da8d57531f1cb081395b1f4460b
Andrew Ferrazzutti <andrewf@element.io>
2023-11-22 09:48:30 -0500

    Update releaser script to check staged contents
    to ensure that the package versions to be committed are correct.

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,19 +7,19 @@ then
     exit 1
 fi
 
-VERSION=`jq -r .version package.json`
+VERSION=`jq -r .version <(git show :package.json)`
 
 function parseCargoVersion {
     awk '$1 == "version" {gsub("\"", "", $3); print $3}' $1
 }
-CARGO_TOML_VERSION=`parseCargoVersion Cargo.toml`
+CARGO_TOML_VERSION=`parseCargoVersion <(git show :Cargo.toml)`
 if [[ $VERSION != $CARGO_TOML_VERSION ]]; then
     echo "Node & Rust package versions do not match." >&2
     echo "Node version (package.json): ${VERSION}" >&2
     echo "Rust version (Cargo.toml): ${CARGO_TOML_VERSION}" >&2
     exit 2
 fi
-CARGO_LOCK_VERSION=`parseCargoVersion <(grep -A1 matrix-hookshot Cargo.lock)`
+CARGO_LOCK_VERSION=`parseCargoVersion <(grep -A1 matrix-hookshot <(git show :Cargo.lock))`
 if [[ $CARGO_TOML_VERSION != $CARGO_LOCK_VERSION ]]; then
     echo "Rust package version does not match the lockfile." >&2
     echo "Rust version (Cargo.toml): ${CARGO_TOML_VERSION}" >&2
