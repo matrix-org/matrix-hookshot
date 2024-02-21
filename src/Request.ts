e19793a7e4dba1091f92781fe082caf429ff0ef0
Half-Shot <will@half-shot.uk>
2024-02-21 14:35:41 +0000

    Use undici upstream fetch, and disable pipling

--- src/Request.ts
+++ src/Request.ts
@@ -1,6 +1,6 @@
 import { setRequestFn } from "matrix-bot-sdk";
 import type { OptionsWithUri, RequestResponse } from "request";
-import { Agent } from "undici";
+import { Agent, fetch as undiciFetch } from "undici";
 
 let installed = false;
 
@@ -13,7 +13,7 @@ export function installRequestFunction() {
         return;
     }
     // Keep connections alive for long enough to be reused.
-    const dispatcher = new Agent({ allowH2: true, keepAliveTimeout: 10000, pipelining: 8 });
+    const dispatcher = new Agent({ allowH2: true });
     const fn = async (params: OptionsWithUri): Promise<{response: RequestResponse, rBody: Buffer|unknown}> => {
         let url = params.uri.toString();
         if (params.qs) {
@@ -21,15 +21,27 @@ export function installRequestFunction() {
         }
         const abort = new AbortController();
         const tOut = params.timeout !== undefined ? setTimeout(() => abort.abort("Timed out"), params.timeout): undefined;
-        const res = await fetch(url, {
-            method: params.method ?? 'GET',
-            body: params.body,
-            headers: params.headers,
-            keepalive: true,
-            dispatcher,
-            signal: tOut ? abort.signal : undefined,
-        } satisfies Partial<RequestInit>);
-        clearTimeout(tOut);
+        let res;
+        try {
+            console.log(params.method ?? "GET", url);
+            res = await undiciFetch(url, {
+                method: params.method ?? 'GET',
+                body: params.body,
+                headers: params.headers,
+                keepalive: true,
+                dispatcher,
+                signal: tOut ? abort.signal : undefined,
+            } satisfies Partial<RequestInit>);
+            console.log("response");
+        } catch (ex) {
+            if (ex instanceof Error && ex.cause) {
+                throw ex.cause;
+            }
+            throw ex;
+        } finally {
+            clearTimeout(tOut);
+        }
+
         let rBody: Buffer|unknown;
         if (res.headers.get('Content-Type') === 'application/json') {
             rBody = await res.json();
@@ -44,7 +56,7 @@ export function installRequestFunction() {
             headers: Object.fromEntries(res.headers.entries()),
             statusCode: res.status,
             statusMessage: res.statusText,
-        } as Partial<RequestResponse> as any};
+        } satisfies Partial<RequestResponse> as any};
     };
     setRequestFn(
         (params: OptionsWithUri, callback: (err: Error|null, response?: any, rBody?: any) => void) => 
