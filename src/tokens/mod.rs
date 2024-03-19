5b166ae186b009db24d07beaa37127dda32a9eef
Will Hunt <will@half-shot.uk>
2024-03-19 17:25:52 +0000

    Remove unwraps / panics

--- src/tokens/mod.rs
+++ src/tokens/mod.rs
@@ -1,24 +1,42 @@
+use std::string::FromUtf8Error;
+
 use base64ct::{Base64, Encoding};
 use napi::bindgen_prelude::Buffer;
+use napi::Error;
 use rsa::pkcs8::DecodePrivateKey;
 use rsa::{Pkcs1v15Encrypt, RsaPrivateKey, RsaPublicKey};
 
 static MAX_TOKEN_PART_SIZE: usize = 128;
 
-pub struct TokenEncryption {
+struct TokenEncryption {
     pub private_key: RsaPrivateKey,
     pub public_key: RsaPublicKey,
 }
 
+#[derive(Debug)]
+enum TokenEncryptionError {
+    FromUtf8(FromUtf8Error),
+    PrivateKey(rsa::pkcs8::Error),
+}
+
+#[derive(Debug)]
+enum DecryptError {
+    Base64(base64ct::Error),
+    Decryption(rsa::Error),
+    FromUtf8(FromUtf8Error),
+}
+
 impl TokenEncryption {
-    pub fn new(private_key_data: Vec<u8>) -> Self {
-        let data = String::from_utf8(private_key_data).unwrap();
-        let private_key = RsaPrivateKey::from_pkcs8_pem(data.as_str()).unwrap();
+    pub fn new(private_key_data: Vec<u8>) -> Result<Self, TokenEncryptionError> {
+        let data =
+            String::from_utf8(private_key_data).map_err(TokenEncryptionError::FromUtf8)?;
+        let private_key = RsaPrivateKey::from_pkcs8_pem(data.as_str())
+            .map_err(TokenEncryptionError::PrivateKey)?;
         let public_key = private_key.to_public_key();
-        TokenEncryption {
+        Ok(TokenEncryption {
             private_key,
             public_key,
-        }
+        })
     }
 }
 
@@ -30,48 +48,65 @@ pub struct JsTokenEncryption {
 #[napi]
 impl JsTokenEncryption {
     #[napi(constructor)]
-    pub fn new(private_key_data: Buffer) -> Self {
+    pub fn new(private_key_data: Buffer) -> Result<Self, Error> {
         let buf: Vec<u8> = private_key_data.into();
-        JsTokenEncryption {
-            inner: TokenEncryption::new(buf),
+        match TokenEncryption::new(buf) {
+            Ok(inner) => Ok(JsTokenEncryption { inner }),
+            Err(err) => Err(Error::new(
+                napi::Status::GenericFailure,
+                format!("Error reading private key: {:?}", err).to_string(),
+            )),
         }
     }
 
     #[napi]
-    pub fn decrypt(&self, parts: Vec<String>) -> String {
+    pub fn decrypt(&self, parts: Vec<String>) -> Result<String, Error> {
         let mut result = String::new();
 
         for v in parts {
-            result += &self.decrypt_value(v);
-        }
-        result
-    }
-
-    pub fn decrypt_value(&self, value: String) -> String {
-        if let Ok(raw_value) = Base64::decode_vec(&value) {
-            if let Ok(decrypted_value) = self.inner.private_key.decrypt(Pkcs1v15Encrypt, &raw_value)
-            {
-                if let Ok(utf8_value) = String::from_utf8(decrypted_value) {
-                    return utf8_value;
-                }
+            let new_value_result = self.decrypt_value(v);
+            if let Ok(new_value) = new_value_result {
+                result += &new_value;
+            } else if let Err(err) = new_value_result {
+                return Err(Error::new(
+                    napi::Status::GenericFailure,
+                    format!("Could not decrypt string: {:?}", err).to_string(),
+                ));
             }
         }
-        panic!("oh no!");
+        Ok(result)
+    }
+
+    fn decrypt_value(&self, value: String) -> Result<String, DecryptError> {
+        let raw_value = Base64::decode_vec(&value).map_err(DecryptError::Base64)?;
+        let decrypted_value = self
+            .inner
+            .private_key
+            .decrypt(Pkcs1v15Encrypt, &raw_value)
+            .map_err(DecryptError::Decryption)?;
+        let utf8_value = String::from_utf8(decrypted_value).map_err(DecryptError::FromUtf8)?;
+        Ok(utf8_value)
     }
 
     #[napi]
-    pub fn encrypt(&self, input: String) -> Vec<String> {
+    pub fn encrypt(&self, input: String) -> Result<Vec<String>, Error> {
         let mut rng = rand::thread_rng();
         let mut parts: Vec<String> = Vec::new();
         for part in input.into_bytes().chunks(MAX_TOKEN_PART_SIZE) {
-            let encrypted = self
+            let encrypted_value = self
                 .inner
                 .public_key
-                .encrypt(&mut rng, Pkcs1v15Encrypt, part)
-                .unwrap();
-            let b64 = Base64::encode_string(encrypted.as_slice());
-            parts.push(b64);
+                .encrypt(&mut rng, Pkcs1v15Encrypt, part);
+            if let Ok(encrypted) = encrypted_value {
+                let b64 = Base64::encode_string(encrypted.as_slice());
+                parts.push(b64);
+            } else if let Err(err) = encrypted_value {
+                return Err(Error::new(
+                    napi::Status::GenericFailure,
+                    format!("Could not encrypt string: {:?}", err).to_string(),
+                ));
+            }
         }
-        parts
+        Ok(parts)
     }
 }
