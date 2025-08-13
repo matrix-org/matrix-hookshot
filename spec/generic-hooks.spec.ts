a2808f7e88e28fe3e4b930b9266c3be3840d62b1
Half-Shot <will@half-shot.uk>
2025-08-13 11:26:00 +0100

    Add a test for room upgrades

--- spec/generic-hooks.spec.ts
+++ spec/generic-hooks.spec.ts
@@ -1,8 +1,4 @@
-import {
-  E2ESetupTestTimeout,
-  E2ETestEnv,
-  E2ETestMatrixClient,
-} from "./util/e2e-test";
+import { E2ESetupTestTimeout, E2ETestEnv } from "./util/e2e-test";
 import {
   describe,
   test,
@@ -13,47 +9,8 @@ import {
   vitest,
 } from "vitest";
 import { GenericHookConnection } from "../src/Connections";
-import { TextualMessageEventContent } from "matrix-bot-sdk";
 import { add } from "date-fns/add";
-
-async function createInboundConnection(
-  user: E2ETestMatrixClient,
-  botMxid: string,
-  roomId: string,
-  duration?: string,
-) {
-  const join = user.waitForRoomJoin({ sender: botMxid, roomId });
-  const connectionEvent = user.waitForRoomEvent({
-    eventType: GenericHookConnection.CanonicalEventType,
-    stateKey: "test",
-    sender: botMxid,
-  });
-  await user.inviteUser(botMxid, roomId);
-  await user.setUserPowerLevel(botMxid, roomId, 50);
-  await join;
-
-  // Note: Here we create the DM proactively so this works across multiple
-  // tests.
-  // Get the DM room so we can get the token.
-  const dmRoomId = await user.dms.getOrCreateDm(botMxid);
-
-  await user.sendText(
-    roomId,
-    "!hookshot webhook test" + (duration ? ` ${duration}` : ""),
-  );
-  // Test the contents of this.
-  await connectionEvent;
-
-  const msgPromise = user.waitForRoomEvent({
-    sender: botMxid,
-    eventType: "m.room.message",
-    roomId: dmRoomId,
-  });
-  const { data: msgData } = await msgPromise;
-  const msgContent = msgData.content as unknown as TextualMessageEventContent;
-  const [_unused1, _unused2, url] = msgContent.body.split("\n");
-  return url;
-}
+import { createInboundConnection } from "./util/helpers";
 
 describe("Inbound (Generic) Webhooks", () => {
   let testEnv: E2ETestEnv;
