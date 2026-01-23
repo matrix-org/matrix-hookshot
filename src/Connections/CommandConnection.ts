34f44962c751f8c230f88d612c25543184eb38fa
Half-Shot <will@half-shot.uk>
2026-01-23 16:28:36 +0000

    Explicitly define help command in SetupConnection

--- src/Connections/CommandConnection.ts
+++ src/Connections/CommandConnection.ts
@@ -31,7 +31,7 @@ export abstract class CommandConnection<
     stateKey: string,
     canonicalStateType: string,
     protected state: ValidatedStateType,
-    private readonly botClient: MatrixClient,
+    protected readonly botClient: MatrixClient,
     private readonly botCommands: BotCommands,
     private readonly helpMessage: HelpFunction,
     protected readonly helpCategories: string[],
